a41795cfac9dfd83ce58b0cf5ac56bac3378f0c4
unknown <erlingc55@gmail.com>
2022-05-18 12:54:22 -0600

    el ejercicio 10 estava facil

---
+++ ejercio10.cs
@@ -0,0 +1,14 @@
+
+
+Double a,b,C
+
+Console.write("ingrese el valor dela variable A :");
+A = Double.parse(Console.ReadLine());
+
+Console.write("ingrese el valor dela variable B :");
+B = double.parse(Console.ReadLine());
+
+C = 180 - (A + B);
+Console.write("el valor del angulo C es de :" + C );
+
+Console.Readkey();
